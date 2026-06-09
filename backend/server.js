4c03031eaf094603b0adfe8a298d9dcc105632d7
Vidya Kudtarkar <spare@Spares-MacBook-Air.local>
2026-06-09 15:15:25 +0530

    Add health endpoint

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "ScoreBoard API Running"
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
